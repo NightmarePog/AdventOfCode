2fc7bb7d6deb5b926eb9cd34d4aa6247c837e9bd
Nightmare Pog <44711407+NightmarePog@users.noreply.github.com>
2025-12-02 17:44:08 +0100

    feat(2025): day one, part two

--- y2025/src/solutions/one.rs
+++ y2025/src/solutions/one.rs
@@ -6,7 +6,7 @@ pub async fn init() {
 }
 
 struct Dial {
-    x: u16,
+    x: i32,
 }
 
 impl Dial {
@@ -14,32 +14,57 @@ impl Dial {
         Dial { x: 50 }
     }
 
-    fn left(&mut self, count: u16) {
-        let new_x = (self.x as i16 - count as i16).rem_euclid(100);
-        self.x = new_x as u16;
+    fn get_position(&self) -> i32 {
+        self.x
     }
 
-    fn right(&mut self, count: u16) {
-        self.x = (self.x + count) % 100;
+    fn left(&mut self, count: u16) -> i32 {
+        let mut click = 0;
+        for _ in 0..count {
+            self.x = (self.x - 1).rem_euclid(100);
+            if self.x == 0 {
+                click += 1;
+            }
+        }
+        click
+    }
+
+    fn right(&mut self, count: u16) -> i32 {
+        let mut click = 0;
+        for _ in 0..count {
+            self.x = (self.x + 1) % 100;
+            if self.x == 0 {
+                click += 1;
+            }
+        }
+        click
     }
 }
 
 fn process(moves: Vec<String>) {
     let mut dial = Dial::new();
-    let mut zero_count = 0;
-    for m in moves {
-        if dial.x == 0 {
-            zero_count += 1;
-        }
-        let direction: char = m.chars().next().unwrap();
-        let count: u16 = m.chars().skip(1).collect::<String>().parse().unwrap();
+    let mut zero_pass_count = 0;
+
+    println!("The dial starts by pointing at 50.");
+
+    for (i, m) in moves.iter().enumerate() {
+        println!("{}", i);
+
+        let direction = m.chars().next().expect("empty line");
+        let count: u16 = m[1..].parse().expect("invalid format");
 
         match direction {
-            'L' => dial.left(count),
-            'R' => dial.right(count),
+            'L' => zero_pass_count += dial.left(count),
+            'R' => zero_pass_count += dial.right(count),
             _ => panic!("Invalid direction"),
         }
-        //println!("final direction: {}", dial.x);
+
+        println!(
+            "The dial is rotated {} to point at {}",
+            m,
+            dial.get_position()
+        );
     }
-    println!("final direction: {}", zero_count);
+
+    println!("to zero it got {}", zero_pass_count);
 }
